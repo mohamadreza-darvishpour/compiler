c6b3b9b8d069d12eaa574b3d6b82dbee6febf2b3
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2023-11-11 16:18:01 -0500

    define symbol table to save all entires.

---
+++ flex_lexi_analyser_part/symb_char_table.h
@@ -0,0 +1,15 @@
+#ifndef SYNB_CHAR_TABLE_H
+#include <iostream>
+#include <vector>
+#endif  SYMB_CHAR_TABLE_H
+
+
+struct character{
+    int number;
+    int line;
+    std:: string char_string;
+    std:: string type;
+};
+
+
+
---
+++ flex_lexi_analyser_part/test.cpp
@@ -0,0 +1,11 @@
+#include "symb_char_table.h"
+#include <iostream>
+#include <vector>
+
+
+int main (){
+    std::vector <character> char_table;
+    char_table.push_back({1,1,"new","new"});
+    std::cout << char_table[0].char_string;
+    return 3482;
+}
